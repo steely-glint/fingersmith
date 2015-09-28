db9c328d32d87ac5b0abc1ddbc5205dfea7745b3
Tim Panton <tim@phonefromhere.com>
2015-09-28 15:19:14 +0100

    Added some stuff from ipseorama - needs extensive edits.

---
+++ phonefromhere/js/dc.js
@@ -0,0 +1,158 @@
+function IpseDataChannel(finger) {
+    var loc = window.location
+    var ws = null;
+    var session;
+    var toFinger;
+    var peerCon;
+    var that = this;
+
+    var makeWs = function(finger) {
+        if (!window.WebSocket) {
+            window.WebSocket = window.MozWebSocket;
+        }
+        if (!window.WebSocket) {
+            alert("Your browser does not support Web Sockets.");
+            return;
+        }
+        var socket, protocol, host, port;
+        protocol = "ws:"
+        if (window.location.protocol === "https:") {
+            protocol = "wss:"
+        }
+        host = window.location.host;
+        port = "";
+        if (window.location.port != "") {
+            port = ":" + window.location.port
+        }
+        socket = new WebSocket(protocol + "//" + host + "/websocket/?finger=" + finger);
+        session = "new"; // fix this
+
+        socket.onopen = function(event) {
+            console.log("wsopen " + JSON.stringify(event));
+        };
+        socket.onclose = function(event) {
+            console.log("wsclose " + JSON.stringify(event));
+            ws = null;
+        };
+        socket.onmessage = function(event) {
+            console.log("message is " + event.data);
+
+            var data = JSON.parse(event.data);
+            console.log("data is " + JSON.stringify(data));
+
+            if (data.session) {
+                var pc = that.peerCon;
+                var sdp = Phono.sdp.buildSDP(data.sdp);
+                console.log("answer sdp is " + sdp);
+                var message = {'sdp': sdp, 'type': data.type};
+                var rtcd;
+                if (typeof mozRTCSessionDescription == "function") {
+                    rtcd = new mozRTCSessionDescription(message);
+                } else {
+                    rtcd = new RTCSessionDescription(message);
+                }
+                console.log("rtcd is " + rtcd);
+                pc.setRemoteDescription(rtcd, function() {
+                    console.log("set " + data.type + " ok");
+                    if (data.type == 'offer') {
+                        var theirfp = JSON.stringify(data.sdp.contents[0].fingerprint.print);
+                        theirfp = theirfp.split(":").join("");
+                        theirfp = theirfp.split('"').join("");
+                        console.log("their fingerprint is " + theirfp)
+                        that.setTo(theirfp);
+                        pc.createAnswer(function(desc) {
+                            pc.setLocalDescription(desc, function() {
+                                console.log("Set Local description " + JSON.stringify(desc));
+                                if (window.showStatus){
+                                   showStatus("Got Answer");
+                                }
+                            }, function(e) {
+                                console.log("Set Local description error "+e);
+                            });
+                        }, function(e) {
+                             console.log("Create answer error "+e );
+                        });
+                    }
+                }, function(e) {
+                        console.log("Set Remote description error "+e);
+                });
+            } else {
+                console.log("no session in my data");
+            }
+        };
+        return socket;
+    };
+    var logError = function(error) {
+        console.log(error.name + ": " + error.message);
+    };
+
+    var withPc = function(pc) {
+// send everything to the peer - via fingersmith
+        pc.onicecandidate = function(evt) {
+            if (evt.candidate === null) {
+                var sdpObj = Phono.sdp.parseSDP(pc.localDescription.sdp);
+                console.log("this.toFinger:" + this.toFinger);
+                console.log("toFinger:" + toFinger);
+                console.log("that.toFinger:" + that.toFinger);
+
+                var sdpcontext = {"to": that.toFinger, "type": pc.localDescription.type, "sdp": sdpObj, "session": session};
+                console.log("sending:" + JSON.stringify(sdpcontext))
+
+                that.ws.send(JSON.stringify(sdpcontext));
+                if (window.showStatus){
+                    showStatus("Sent offer.");
+                }
+            } else {
+                console.log("ignoring local trickling candidates for now")
+                if (window.showStatus){
+                    showStatus("Gathering candidates.");
+                }
+            }
+        };
+        // let the "negotiationneeded" event trigger offer generation
+        pc.onnegotiationneeded = function() {
+            var sdpConstraints = {'mandatory': {'OfferToReceiveAudio': false, 'OfferToReceiveVideo': false}}
+            pc.createOffer(function(desc) {
+                pc.setLocalDescription(desc, function() {
+                    console.log("Set Local description");
+                }, logError);
+            }, logError, sdpConstraints);
+        }
+        that.peerCon = pc;
+        pc.ondatachannel = function(evt){
+            if (that.ondatachannel){
+                that.ondatachannel(evt);
+            }
+        };
+        that.ws = makeWs(finger);
+    }
+    var configuration = {"iceServers": [
+            {url: "stun:stun.l.google.com:19302"}//,
+        ]};
+    if (typeof webkitRTCPeerConnection == "function") {
+        var pc = new webkitRTCPeerConnection(configuration, null);
+        withPc(pc);
+    } else if (typeof mozRTCPeerConnection == "function") {
+        Ipseorama.addMyCertToPeerConf(configuration, function() {
+            var mozpc = new mozRTCPeerConnection(configuration, null)
+            withPc(mozpc);
+        });
+    }
+}
+
+IpseDataChannel.prototype.createDataChannel = function(name, props) {
+    return this.peerCon.createDataChannel(name, props)
+}
+IpseDataChannel.prototype.setTo = function(tof) {
+    this.toFinger = tof;
+    var d = new Date();
+    var n = d.getTime();
+    this.session = this.finger + "-" + tof + "-" + n; // fix this
+    console.log("this.toFinger:" + this.toFinger);
+}
+IpseDataChannel.prototype.setOnDataChannel = function(callback) {
+    this.ondatachannel = function(evt) {
+        callback(evt.channel);
+    };
+}
+
---
+++ phonefromhere/js/ipseorama.js
@@ -0,0 +1,128 @@
+/* 
+ * soucecode copyright Westhawk Ltd 2014 - all rights reserved.
+ */
+
+;
+(function() {
+    Ipseorama = {
+        db: null,
+        createCert: function(app, doneCB) {
+            console.log("create a new cert");
+            var creq = mozRTCPeerConnection.generateCertificate({name: "ECDSA", namedCurve: "P-256"})
+ //           var creq = mozRTCPeerConnection.generateCertificate({name: "RSA"})
+
+            creq.then(function(cert) {
+                console.log("created a new cert, now store it.");
+                var tx = Ipseorama.db.transaction("IpseCert", "readwrite");
+                tx.oncomplete = function() {
+                    doneCB(cert);
+                    console.log("transaction done .");
+                };
+                tx.onerror = function(event) {
+                    console.log("transaction error is "+event.target.error.message);
+                };
+                var store = tx.objectStore("IpseCert");
+                var updateRequest = store.put({app: app, cert: cert, timestamp: Date.now()});
+                updateRequest.onsuccess =  function() {
+                    console.log("cert stored.");
+                };
+                updateRequest.onerror = function(event) {
+                    console.log("update error is "+event.target.error.message);
+                };
+
+            }
+            );
+
+        },
+        findOrCreateCert: function(app, doneCB) {
+            var tx = Ipseorama.db.transaction("IpseCert", "readonly");
+            var store = tx.objectStore("IpseCert");
+            var index = store.index("by_app");
+            console.log("Looking for cert in Indexdb");
+
+            var request = index.get(app);
+            request.onsuccess = function(ev) {
+                var matching = this.result;
+                if (matching) {
+                    console.log("Returning matched cert in DB");
+                    doneCB(matching.cert);
+                } else {
+                    console.log("No suitable cert in DB - creating one ");
+                    Ipseorama.createCert(app, doneCB);
+                }
+            };
+        },
+        findOrCreateCertAndDB: function(app, doneCB) {
+            if (Ipseorama.db == null) {
+                var request = indexedDB.open("IpseCert",4);
+                request.onupgradeneeded = function() {
+                    var db = request.result;
+                    console.log("Indexdb.open() needed upgrade...");
+
+                    // The database did not previously exist, so create object stores and indexes. var db = request.result;
+                    var store = db.createObjectStore("IpseCert", {keyPath: "app"});
+                    var appIndex = store.createIndex("by_app", "app");
+                };
+                request.onsuccess = function() {
+                    console.log("Indexdb.open() ok");
+                    Ipseorama.db = request.result;
+                    Ipseorama.findOrCreateCert(app, doneCB);
+                };
+                request.onerror = function(event) {
+                    console.log("Indexdb.open() error is "+event.target.error.message);
+                };
+            } else {
+                Ipseorama.findOrCreateCert(app, doneCB);
+            }
+        },
+        addMyCertToPeerConf: function(peerconf, mkpc) {
+            Ipseorama.findOrCreateCertAndDB("test",
+                    function(cert) {
+                        console.log("got cert" + cert);
+                        peerconf.certificates = [cert];
+                        mkpc();
+                    }
+            );
+        },
+        getFinger: function(descsdp) {
+            var sdp = Phono.sdp.parseSDP(descsdp)
+            var myfp = JSON.stringify(sdp.contents[0].fingerprint.print);
+            myfp = myfp.split(":").join("");
+            myfp = myfp.split('"').join("");
+            console.log("fingerprint is " + myfp)
+            return myfp;
+        },
+        whoAmI: function(okCB, failCB) {
+            var peerconfig = {"iceServers": [{url: "stun:stun.l.google.com:19302"}]};
+            var offerCreated = function(localDesc) {
+                var myfp = Ipseorama.getFinger(localDesc.sdp)
+                okCB(myfp);
+            }
+            var withPc = function(pc) {
+                pc.createDataChannel('channel', {});
+                pc.createOffer(offerCreated,
+                        function(e) {
+                            failCB("Couldn't creat offer")
+                        }
+                        /*,{mandatory: {
+                                OfferToReceiveVideo: false,
+                                OfferToReceiveAudio: false,
+                            }}*/);
+            }
+            if (typeof webkitRTCPeerConnection == "function") {
+                var wcpc = new webkitRTCPeerConnection(peerconfig, null);
+                withPc(wcpc);
+            } else if (typeof mozRTCPeerConnection == "function") {
+                if (typeof mozRTCPeerConnection.generateCertificate == "function") {
+                    Ipseorama.addMyCertToPeerConf(peerconfig, function() {
+                        var mozpc = new mozRTCPeerConnection(peerconfig, null)
+                        withPc(mozpc);
+                    });
+                }
+            }
+
+        }
+    }
+}());
+
+
