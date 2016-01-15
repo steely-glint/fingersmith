6f6dc5c540f9ec0197c830c9d8595cd498e7416e
Tim Panton <tim@phonefromhere.com>
2016-01-15 21:24:57 +0000

    Added tbone - temp monitor for edison etc.

--- src/main/scala/com/ipseorama/messageExchange/FingerSmithApp.scala
+++ src/main/scala/com/ipseorama/messageExchange/FingerSmithApp.scala
@@ -118,6 +118,10 @@ object FingerSmithApp extends Logger {
         // Return HTML page to establish web socket
         staticContentHandlerRouter ! new StaticResourceRequest(httpRequest,"vbone.html")
       }
+      case GET(Path("/tbone.html")) => {
+        // Return HTML page to establish web socket
+        staticContentHandlerRouter ! new StaticResourceRequest(httpRequest,"tbone.html")
+      }
       case GET(Path("/brick.html")) => {
         // Return HTML page to establish web socket
         staticContentHandlerRouter ! new StaticResourceRequest(httpRequest,"brick.html")
